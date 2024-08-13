49210c7b50964e0f1f4569fa915cfc98a4f848b6
Gowtham Kanakasabapathy <gowtham4intc@gmail.com>
2024-08-13 00:30:52 -0700

    add new button

--- frontend/app.js
+++ frontend/app.js
@@ -19,3 +19,21 @@ document.getElementById('commandForm').addEventListener('submit', async (event) 
 document.getElementById('clearScreenButton').addEventListener('click', () => {
     document.getElementById('result').textContent = '';
 });
+
+
+document.getElementById('running-vm').addEventListener('click', async () => {
+    const hardcodedCommand = 'gcloud compute instances list --filter="status:RUNNING" --format="table(name, creationTimestamp)"'; // Replace this with your hardcoded command
+    try {
+        const response = await fetch('http://localhost:5000/execute-command', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ command: hardcodedCommand }),
+        });
+        const data = await response.json();
+        document.getElementById('result').textContent = data.result || data.error;
+    } catch (err) {
+        document.getElementById('result').textContent = 'Error: ' + err.message;
+    }
+});
